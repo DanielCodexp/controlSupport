76df8654d6716ad338d9e3d3034275bc778532b9
Dhanielxp98 <116531997+DanielCodexp@users.noreply.github.com>
2023-01-15 01:51:29 -0600

    cambios

---
+++ src/app/interface/cars-interface.ts
@@ -0,0 +1,16 @@
+
+
+export interface DatosCars{
+    id:string
+    airConditioner:boolean;
+    available:boolean;
+    carBrand:string;
+    largeBaggage:number;
+    price_day:number;
+    smallBaggage:number;
+    smoke:boolean;
+    type:string;
+    url:string;
+    persons:number;
+    chofer:boolean
+}
