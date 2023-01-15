9e1f797cfb6081717e82544524648aa658ddf953
Edgar18121995 <96259301+Edgar18121995@users.noreply.github.com>
2023-01-15 01:51:36 -0600

    se agrego conexion firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'support-8225a',
+    appId: '1:1026321055747:web:4ceaad81ec0ac73a46ce36',
+    storageBucket: 'support-8225a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyC7nuDlJyhLcj40L3bN1UEUP-CmwXUXnAI',
+    authDomain: 'support-8225a.firebaseapp.com',
+    messagingSenderId: '1026321055747',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'support-8225a',
+    appId: '1:1026321055747:web:4ceaad81ec0ac73a46ce36',
+    storageBucket: 'support-8225a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyC7nuDlJyhLcj40L3bN1UEUP-CmwXUXnAI',
+    authDomain: 'support-8225a.firebaseapp.com',
+    messagingSenderId: '1026321055747',
+  },
   production: false
 };
 
